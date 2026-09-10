7835409ba6994c643fe0a0eb2e0af587bf62c06a
Fagarester <denis.volodin.1992@gmail.com>
2026-09-10 22:26:15 +0100

    Add missing repositories configuration to root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,10 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
