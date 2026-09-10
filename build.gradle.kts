29ce1adc24750eb3733715fea91173c3b049083b
Fagarester <denis.volodin.1992@gmail.com>
2026-09-10 22:31:42 +0100

    Fix: Remove subprojects repositories block to comply with PREFER_SETTINGS mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
-
-subprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
